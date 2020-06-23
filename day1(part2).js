7641652c329ada534f6fc29830ce313078472140
gauriguglani11 <gauriguglani2@gmail.com>
2020-06-23 20:07:43 +0530

    Functions

---
+++ day1(part2).js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+FIND FACTORIAL
+
+
+CODE:-
+/*
+ * Create the function factorial here
+ */
+function factorial(n){
+    if(n === 0){
+        return 1;
+    } else{
+        return n * factorial(n-1);
+    }
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
